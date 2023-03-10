5ae8328d16d2a877400e363cad9fe02ede12d5fd
Vikash2806 <vikashrajendran2806@gmail.com>
2023-03-10 17:36:53 +0530

    feat: state wise GDP section add

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,3 +14,20 @@ if (divElement.offsetWidth > 800) {
 var scriptElement = document.createElement("script");
 scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
 vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+var divElement = document.getElementById("viz1678444713964");
+var vizElement = divElement.getElementsByTagName("object")[0];
+if (divElement.offsetWidth > 800) {
+  vizElement.style.width = "1000px";
+  vizElement.style.height = "827px";
+  vizElement.style.borderRadius = "10px";
+} else if (divElement.offsetWidth > 500) {
+  vizElement.style.width = "1000px";
+  vizElement.style.height = "827px";
+} else {
+  vizElement.style.width = "100%";
+  vizElement.style.height = "727px";
+}
+var scriptElement = document.createElement("script");
+scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
